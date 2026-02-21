5daf0e994eb2cfc6235a88de137a29546ee37666
DG <gokanidennis@gmail.com>
2026-02-21 08:59:01 +0530

    added more structure padding examples

--- Base/data-types/05_structures.c
+++ Base/data-types/05_structures.c
@@ -20,6 +20,30 @@ struct StructWithPadding_01 {
     int y;      // This will be aligned to a 4-byte boundary, causing padding before it 
 };
 
+// Another structure to demonstrate padding with different member order
+struct StructWithPadding_02 {
+    char c;     // This will cause padding to be added after it
+    char x;     // This will cause padding to be added after it
+    char z;     // This will cause padding to be added after it
+};
+
+// Another structure to demonstrate padding with different member order
+struct StructWithPadding_03 {
+    char c;     // This will cause padding to be added after it
+    char x;     // This will cause padding to be added after it
+    char z;     // This will cause padding to be added after it
+    int y;      // This will be aligned to a 4-byte boundary, causing padding before it
+};
+
+// Another structure to demonstrate padding with different member order
+struct StructWithPadding_04 {
+    char a;     // This will cause padding to be added after it
+    char b;     // This will cause padding to be added after it
+    char c;     // This will cause padding to be added after it
+    char d;     // This will cause padding to be added after it
+    int y;      // This will be aligned to a 4-byte boundary, causing padding before it
+};
+
 // Main function to demonstrate the use of structures
 int main() {
     struct Point p1 = {10, 20};
@@ -38,5 +62,17 @@ int main() {
     printf("Struct with padding 01: c=%c, x=%c, y=%d\n", sp01.c, sp01.x, sp01.y);
     printf("Size of StructWithPadding_01 structure: %lu bytes\n", sizeof(struct StructWithPadding_01));
 
+    struct StructWithPadding_02 sp02 = {'D', 'E', 'F'};
+    printf("Struct with padding 02: c=%c, x=%c, z=%c\n", sp02.c, sp02.x, sp02.z);
+    printf("Size of StructWithPadding_02 structure: %lu bytes\n", sizeof(struct StructWithPadding_02));
+
+    struct StructWithPadding_03 sp03 = {'G', 'H', 'I', 5};
+    printf("Struct with padding 03: c=%c, x=%c, z=%c, y=%d\n", sp03.c, sp03.x, sp03.z, sp03.y);
+    printf("Size of StructWithPadding_03 structure: %lu bytes\n", sizeof(struct StructWithPadding_03)); 
+
+    struct StructWithPadding_04 sp04 = {'J', 'K', 'L', 'M', 6};
+    printf("Struct with padding 04: a=%c, b=%c, c=%c, d=%c, y=%d\n", sp04.a, sp04.b, sp04.c, sp04.d, sp04.y);
+    printf("Size of StructWithPadding_04 structure: %lu bytes\n", sizeof(struct StructWithPadding_04)); 
+    
     return 0;
 }
